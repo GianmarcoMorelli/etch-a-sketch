1cf3ee714e3fe25c22fce6c02ac34a9831a957a0
GianmarcoMorelli <gianmarco.morelli96@gmail.com>
2023-01-24 11:04:46 +0100

    Prompt fro user added

--- javascript.js
+++ javascript.js
@@ -1,5 +1,27 @@
-const mainContainer = document.querySelector('.container');
+const mainContainer = document.querySelector(".container");
+const theButton = document.querySelector("button");
+let gridDimension;
+let newDivDimension;
 let i = 0;
+
+theButton.addEventListener("click", funcPrompt);
+
+function funcPrompt(){
+    gridDimension = parseInt(prompt("Please enter the desired dimension of the grid (max 100 x 100)","16"));
+    mainContainer.innerHTML = "";
+    if(gridDimension <= 100){
+        for(i = 0; i < gridDimension*gridDimension; i++){
+            mainContainer.innerHTML += "<div class=\"myGrid\">Prova</div>";
+        }
+        newDivDimension = 100 / gridDimension;
+        console.log(newDivDimension);
+        const mySingDiv = document.querySelectorAll(".myGrid");
+        mySingDiv.forEach(element => {
+            element.style.height = newDivDimension + "%";
+            element.style.width = newDivDimension + "%";
+        });
+    }
+}
 
 for(i = 0; i < 16*16; i++){
     mainContainer.innerHTML += "<div class=\"myGrid\">Prova</div>";
